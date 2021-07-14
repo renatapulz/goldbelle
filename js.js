a13f245bcc08b7c8746da67e86ab63144cb1c85d
Renata Pulz <84743054+renatapulz@users.noreply.github.com>
2021-07-14 01:09:10 -0300

    feito formulario

--- js.js
+++ js.js
@@ -1,1 +1,47 @@
-//oi
+// VALIDACAO CPF
+
+let chamaValidaCPF = (param) => {
+    let check = validaCPF(param);
+    let elemento = document.getElementById("cpf");
+ 
+    if (check == true){
+       elemento.classList.remove("border-danger");
+       elemento.classList.add("border-success");
+       return true;
+    }
+
+    elemento.classList.add("border-danger");
+    return false;
+ }
+ 
+ let validaCPF = (strCPF) => {
+    let Soma;
+    let Resto;
+    Soma = 0;
+    
+    if (strCPF == "") return null;
+    if (strCPF == "00000000000") return false;
+ 
+ 
+    for (i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+ 
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+ 
+    Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+ 
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+    return true;
+ }
+
+  const submitForm = () => {
+    const cpf = document.getElementById('cpf').value;
+
+    if (chamaValidaCPF(cpf)) {
+      document.getElementById('formcadastro').innerHTML = "Cadastro enviado!";
+    }
+  }
